47a3bd389c3d831955be2171afc650c7ca818c87
Serkan UYSAL <serqkanseko@gmail.com>
2019-01-20 01:05:52 +0300

    Create Factorial.cpp
    This program gives the factorial of the numbers.

---
+++ Factorial.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int fact(int);
+int main(){
+    cout << "Please enter a positive integer: ";
+    int user,final;
+    cin >> user;
+    cout <<"Your factorial of number is: "<<fact(user)<<endl;
+}
+
+int fact(int num){
+    if(num==1)
+        return 1;
+    return num*fact(num-1);
+}
