b6f0503180463a065f105da2ac4b8dc58c4070e2
Serkan UYSAL <serqkanseko@gmail.com>
2019-05-30 23:10:06 +0300

    minik eklemeler

---
+++ dnm.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout <<__FILE__<<endl<<__TIME__<<endl<<__DATE__<<endl<<__LINE__<<endl<<__cplusplus<<endl<<__TIMESTAMP__<<endl<<__BASE_FILE__<<endl<<__USE_GNU<<endl;
+}
